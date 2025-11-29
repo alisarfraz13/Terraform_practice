8fdf65c5e97d4c873c14adee5ce34fc3ea04e0e8
alisarfraz13 <alisarfraz6411@gmail.com>
2025-11-29 19:50:39 +0500

    Terraform updates from VS Code (without .terraform providers)

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
-terraform{
-
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.23.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "firstserver" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sample-instance"
+  }
+}
+
+
